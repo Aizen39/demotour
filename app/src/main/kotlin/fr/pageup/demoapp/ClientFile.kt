dae64017c159ac98118987a2cceff19b92d1c0cf
Anthony HO <aho@pageup.fr>
2021-10-01 10:18:25 +0200

    SplashScreen fini

--- app/src/main/kotlin/fr/pageup/demoapp/ClientFile.kt
+++ app/src/main/kotlin/fr/pageup/demoapp/ClientFile.kt
@@ -1,9 +1,3 @@
 package fr.pageup.demoapp
 
-data class Customer(val id:Long, val firstname: String, val lastname: String) {
-
-
-    val client1 = Customer(6884567,"rogers", "steve",)
-    val client2 = Customer(6887892,"black", "widow",)
-
-}
+data class Customer(val id:Long, val firstname: String, val lastname: String)
