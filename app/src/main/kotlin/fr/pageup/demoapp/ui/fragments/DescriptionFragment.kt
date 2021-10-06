19b93a46318d174f4a2ce12d245b43b285fcd32a
Anthony HO <aho@pageup.fr>
2021-10-06 09:48:34 +0200

    design new fragment for description

--- app/src/main/kotlin/fr/pageup/demoapp/ui/fragments/DescriptionFragment.kt
+++ app/src/main/kotlin/fr/pageup/demoapp/ui/fragments/DescriptionFragment.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import fr.pageup.demoapp.R
 
 class DescriptionFragment : Fragment(){
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_description, container,false)
     }
 }
--- app/src/main/kotlin/fr/pageup/demoapp/ui/fragments/LoginFragment.kt
+++ app/src/main/kotlin/fr/pageup/demoapp/ui/fragments/LoginFragment.kt
@@ -10,15 +10,22 @@ import androidx.navigation.findNavController
 import fr.pageup.demoapp.R
 import fr.pageup.demoapp.databinding.FragmentListItemBinding
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
 
     //Create login fragment
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentListItemBinding>(inflater,
-            R.layout.fragment_list_item,container,false)
-        binding.signIn.setOnClickListener {
-            view:View -> view.findNavController().navigate(R.id.action_demoTourLoginFragment_to_demoTourListFragment)
+        val binding = DataBindingUtil.inflate<FragmentListItemBinding>(
+            inflater,
+            R.layout.fragment_list_item, container, false
+        )
+        binding.signIn.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(R.id.action_demoTourLoginFragment_to_demoTourListFragment)
         }
         return binding.root
 
