e34bc39bb3ca1d4cd4b50453a2c5687b18af2995
qandrieu <qandrieu@pageup.fr>
2021-10-19 11:19:26 +0200

    Revert type T for interface OnItemClickListener

--- app/src/main/kotlin/fr/pageup/demoapp/ui/adapters/OnItemClickListener.kt
+++ app/src/main/kotlin/fr/pageup/demoapp/ui/adapters/OnItemClickListener.kt
@@ -3,5 +3,5 @@ package fr.pageup.demoapp.ui.adapters
 import fr.pageup.demoapp.data.Customer
 
 interface OnItemClickListener<T> {
-    fun onItemClick(item: Customer)
+    fun onItemClick(item: T)
 }
