9fd0e7b648571824b1b14d502844a2c5f4908487
Anthony HO <aho@pageup.fr>
2021-10-21 17:12:43 +0200

    add service provider

---
+++ app/src/main/kotlin/fr/pageup/demoapp/data/remote/Service provider.kt
@@ -0,0 +1,21 @@
+package fr.pageup.demoapp.data.remote
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import fr.pageup.demoapp.data.model.Customer
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ServiceProvider {
+
+    private const val BASE_URL = "http://172.31.99.30:5000/"
+    //create moshi object
+    private val moshi = Moshi.Builder().build()
+    //create adapter
+    private val adapter: JsonAdapter<Customer> = moshi.adapter(Customer::class.java)
+
+    val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+}
